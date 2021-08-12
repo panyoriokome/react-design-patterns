e4f3389e7024cf4a834bc3297978feb377afde32
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-08-12 20:28:40 +0900

    first commit

---
+++ my-app/src/pages/index.tsx
@@ -0,0 +1,184 @@
+import Image from 'next/image'
+import { css } from 'twin.macro'
+
+export const Home = () => {
+  return (
+    <div css={container}>
+      <section css={main}>
+        <h1 css={[title, title_and_description]}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p css={[description, title_and_description]}>
+          Get started by editing <code>pages/index.tsx</code>
+        </p>
+
+        <button
+          onClick={() => {
+            window.alert(
+              'With TypeScript, ESlint, Jest, Emotion, Tailwind and Twin'
+            )
+          }}
+        >
+          Test Button
+      </button>
+
+        <div css={grid}>
+          <a href="https://nextjs.org/docs" css={[a, card]}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" css={[a, card]}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            css={[a, card]}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            css={[a, card]}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          </a>
+        </div>
+      </section>
+
+      <footer css={footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+          css={a}
+        >
+          Powered by{' '}
+          <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+const container = css`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const main = css`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const footer = css`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    margin-left: 0.5rem;
+  }
+
+  & a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const a = css`
+  color: inherit;
+  text-decoration: none;
+`
+
+const title = css`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+
+  & a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+
+  & a:hover,
+  & a:focus,
+  & a:active {
+    text-decoration: underline;
+  }
+`
+
+const title_and_description = css`
+  text-align: center;
+`
+
+const description = css`
+  line-height: 1.5;
+  font-size: 1.5rem;
+`
+
+const grid = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+
+  max-width: 800px;
+  margin-top: 3rem;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`
+
+const card = css`
+  margin: 1rem;
+  flex-basis: 45%;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+
+  & h3 {
+    margin: 0 0 1rem 0;
+    font-size: 1.5rem;
+  }
+
+  & p {
+    margin: 0;
+    font-size: 1.25rem;
+    line-height: 1.5;
+  }
+`
+
+
+export default Home
