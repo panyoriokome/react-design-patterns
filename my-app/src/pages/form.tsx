ab9230b8fdda4a4dd6ec91763c0333533fc1aae5
Yusuke_Watanabe <yuu-watanabe@ois.okasan.co.jp>
2021-08-25 12:35:31 +0900

    エラーメッセージを日本語化

--- my-app/src/pages/form.tsx
+++ my-app/src/pages/form.tsx
@@ -4,23 +4,68 @@ import { Controller, useForm, FormProvider } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 
+yup.setLocale({
+  mixed: {
+    default: '入力エラーです',
+    required: '必須入力項目です',
+    oneOf: ({ values }) => `次の値のいずれかでなければなりません: ${values}`,
+    notOneOf: ({ values }) => `次の値のいずれかであってはなりません: ${values}`,
+    notType: '形式が違います',
+  },
+  string: {
+    length: ({ length }) => `${length}文字入力して下さい`,
+    min: ({ min }) => `${min}文字以上入力してください`,
+    max: ({ max }) => `${max}文字以内で入力して下さい`,
+    matches: '形式が違います',
+    email: '形式が違います',
+    url: '形式が違います',
+    trim: '前後にスペースを入れてはいけません',
+    lowercase: '小文字でなければなりません',
+    uppercase: '大文字でなければなりません',
+  },
+  number: {
+    min: ({ min }) => `${min}以上の値を入力して下さい`,
+    max: ({ max }) => `${max}以下の値を入力して下さい`,
+    lessThan: ({ lessThan }) => `${lessThan}より小さい値を入力して下さい`,
+    moreThan: ({ moreThan }) => `${moreThan}より大きい値を入力して下さい`,
+    notEqual: ({ notEqual }) => `${notEqual}と違う値を入力して下さい`,
+    positive: '正の数を入力して下さい',
+    negative: '負の数を入力して下さい',
+    integer: '整数を入力して下さい',
+  },
+  date: {
+    default: '形式が違います',
+    min: ({ min }) => `${min}以上の日付を入力して下さい`,
+    max: ({ max }) => `${max}以下の日付を入力して下さい`,
+  },
+  object: {
+    noUnknown: '有効なキーを持ったデータを入力して下さい',
+  },
+  array: {
+    min: ({ min }) => `${min}個以上の値を入力して下さい`,
+    max: ({ max }) => `${max}個以下の値を入力して下さい`,
+  },
+});
+
 const schema = yup.object().shape({
   username: yup.string().required(),
   firstName: yup.string().required(),
   age: yup.number().positive().integer().required(),
 });
 
-// const defaultValues = {
-//   username: '初期値',
-//   firstName: null,
-//   age: null
-// }
+
+
+const defaultValues = {
+  username: '初期値',
+  firstName: null,
+  age: null
+}
 
 export const Form = () => {
   const methods = useForm({
     resolver: yupResolver(schema),
     mode: "onBlur",
-    // defaultValues: defaultValues
+    defaultValues: defaultValues
   })
   const { handleSubmit, formState: { errors }, register } = methods
   return (
